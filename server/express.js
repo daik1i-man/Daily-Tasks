45c1601f6c3a81009ab2e13681bc4b950c3ca71c
Abdushukur <abduhakimovabdushukur@gmail.com>
2024-05-02 19:50:52 +0500

    update verification

--- server/express.js
+++ server/express.js
@@ -37,22 +37,6 @@ app.use(session({
 app.use("/user", userAuth)
 app.use("/tasks", tasks)
 
-
-
-app.get("/verification/:token", async(req, res)=>{
-    const token = req.params.token;
-  try {
-    const user = await db.query("SELECT * FROM users WHERE verification_token = $1", [token]);
-    if (user.rows.length === 0) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    await db.query("UPDATE users SET verified = true, verification_token = NULL WHERE id = $1", [user.rows[0].id]);
-    res.send("Email verified successfully");
-  } catch (err) {
-    console.error("Error during email verification:", err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-})
 app.listen(PORT , () =>{
     console.log(`http://localhost:${PORT}`);
 })
