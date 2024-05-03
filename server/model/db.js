91b35f1803368e9f5fb50d01e69c48dc66564947
Alimboyev Joshqin <138476853+daikii-man@users.noreply.github.com>
2024-05-03 10:41:45 +0500

    Update db.js

--- server/model/db.js
+++ server/model/db.js
@@ -8,14 +8,5 @@ const db = new Pool({
     database: "bjeet2l1rpkjqzk2qrrc"
 })
 
-const {Pool} = require("pg")
-
-const db = new Pool({
-    host:"bjeet2l1rpkjqzk2qrrc-postgresql.services.clever-cloud.com",
-    port:"50013",
-    user:"ufa7jrdrir2csoh1qvju",
-    password:"4uOA5nRciItWs6EXygEWQzO0VwyGjG",
-    database:"bjeet2l1rpkjqzk2qrrc"
-})
 
 module.exports = db
